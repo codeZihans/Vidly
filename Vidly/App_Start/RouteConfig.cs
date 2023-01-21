a67d583ed78869395768208c13144cdbc0b34a15
Zahangir Alam <zahangirdu1@gmail.com>
2023-01-21 17:28:43 +0600

    Init

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //route path declared in Route attribute in controller actions
+            routes.MapMvcAttributeRoutes();
+
+            //default route
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie
+            {
+                Id = 1,
+                Name = "Shrek!"
+            };
+            ViewData["RandomMovie"] = movie;
+            ViewBag.Movie = movie;
+            return View();
+        }
+
+        //Custom Route used by 
+        //routes.MapMvcAttributeRoutes();
+        /*
+         * month with 2 digits and among 1-12
+         * */
+        [Route("movies/released/{year:regex(\\d{4}):range(2012,2015)}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
